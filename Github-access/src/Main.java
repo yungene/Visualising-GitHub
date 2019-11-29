2c1486267959bfa00240ee0c5ea6d5d0fce79bf1
yungene <32654121+yungene@users.noreply.github.com>
2019-11-29 23:54:57 +0000

    Log in user using oauth2 token.

--- Github-access/src/Main.java
+++ Github-access/src/Main.java
@@ -1,14 +1,21 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.RepositoryService;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			RepositoryService service = new RepositoryService();
-			for (Repository repo : service.getRepositories("yungene"))
+			GitHubClient client = new GitHubClient();
+			Scanner reader = new Scanner(System.in);
+			System.out.print("Input your OAuth token: ");
+			String token = reader.next();
+			client.setOAuth2Token(token);
+			RepositoryService service = new RepositoryService(client);
+			for (Repository repo : service.getRepositories())
 				System.out.println(repo.getName() + " Watchers: " + repo.getWatchers());
 		} catch (IOException e) {
 			e.printStackTrace();
